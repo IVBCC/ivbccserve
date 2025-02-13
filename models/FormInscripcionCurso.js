d2032f3b850c67b3c21b941c9b08b249bd91cac9
JesusDHernandezR <jesusdavidhernandez@unicesar.edu.co>
2025-02-12 23:39:34 -0500

    validacion telefono

--- models/FormInscripcionCurso.js
+++ models/FormInscripcionCurso.js
@@ -1,13 +1,20 @@
-const  mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 
 const formInscripcionCursoSchema = new mongoose.Schema({
     curso: { type: String, required: true },
     nombreCompleto: { type: String, required: true },
     correo: { type: String, required: true },
-    telefono: { type: Number, required: true },
+    telefono: {
+        type: Number, required: true, validate: {
+            validator: function (v) {
+                return /^\d{10,}$/.test(v);
+            },
+            message: "El número de teléfono debe tener al menos 10 dígitos",
+        }
+    },
     sexo: { type: String, required: true },
     edad: { type: Number, required: true },
     comentario: { type: String, required: true },
 })
 
-module.exports = mongoose.model('FormInscripcionCurso',formInscripcionCursoSchema);
+module.exports = mongoose.model('FormInscripcionCurso', formInscripcionCursoSchema);
