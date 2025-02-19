ac78e76cca36cc30de6e1c9244aa1e02c4066cd1
JesusDHernandezR <jesusdavidhernandez@unicesar.edu.co>
2025-02-19 13:53:23 -0500

    restauracion

--- routes/excel.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const saveToGoogleSheet = require("../controllers/excelController");
-
-router.post("/guardar-en-google", saveToGoogleSheet);
-
-module.exports = router;
