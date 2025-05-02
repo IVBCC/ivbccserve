524ba670e80d9c501386c514d898d0e6bce6f6d1
JesusDHernandezR <jesusdavidhernandez@unicesar.edu.co>
2025-05-01 21:24:36 -0500

    archivo json

--- routes/iglesiasRouter.js
+++ routes/iglesiasRouter.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    fs.readFile('./data-api/iglesias.json', 'utf8', (err, data) => {
+    const filePath = path.join(process.cwd(), 'data-api', 'iglesias.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
+            console.error("Error al leer el archivo:", err.message);
             return res.status(500).json({ error: 'No se pudo leer el archivo' });
         }
         res.json(JSON.parse(data));
